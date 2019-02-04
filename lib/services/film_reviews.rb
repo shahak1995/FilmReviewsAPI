b8648d5bdefa7f78add4d4ee94389fc317625c1e
Tech-A25 <Tech-A25@Tech-A25s-MacBook-Air.local>
2019-02-04 13:34:57 +0000

    Did the loops for the test and methods to check each of the arrays and hashes for specific data

--- lib/services/film_reviews.rb
+++ lib/services/film_reviews.rb
@@ -11,14 +11,82 @@ class FilmReviews
   
   def retrieve_film_reviews(film_name)
     api_key = ENV['REVIEW_API_KEY']
-    @latest_film_reviews = JSON.parse(self.class.get("/svc/movies/v2/reviews/search.json?query=#{film_name}&api-key=#{api_key}").body)
+    @latest_film_data = JSON.parse(self.class.get("/svc/movies/v2/reviews/search.json?query=#{film_name}&api-key=#{api_key}").body)
   end 
 
-  def print_all_film_reviews
-    @latest_film_reviews
+  def retrieve_film_results
+    @latest_film_data
   end 
+
+  def retrieve_status
+    @latest_film_data['status']
+  end 
+
+  def retrieve_copyright
+    @latest_film_data['copyright']
+  end 
+
+  # def retrieve_has_more
+  #   @latest_film_data['has_more']
+  # end 
+
+  def retrieve_num_results
+    @latest_film_data['num_results']
+  end 
+
+  def retrieve_results
+    @latest_film_data['results']
+  end 
+
+  def retrieve_all_display_title
+    display_title = []
+    retrieve_results.each do |title|
+      display_title << title['display_title']
+    end 
+    display_title
+  end
+
+  def retrieve_mpaa_rating
+    mpaa_rating = []
+    retrieve_results.each do |rating|
+      mpaa_rating << rating['mpaa_rating']
+    end 
+    mpaa_rating
+  end
+
+  def retrieve_critics_pick
+    critics_pick = []
+    retrieve_results.each do |pick|
+      critics_pick << pick['critics_pick']
+    end 
+    critics_pick
+  end
+
+  def retrieve_byline
+    byline = []
+    retrieve_results.each do |line|
+      byline << line['byline']
+    end 
+    byline
+  end
+
+  def retrieve_headline
+    headline = []
+    retrieve_results.each do |head|
+      headline << head['headline']
+    end 
+    headline
+  end
+
+  def retrieve_summary_short
+    summary_short = []
+    retrieve_results.each do |short|
+      summary_short << short['summary_short']
+    end 
+    summary_short
+  end
 end 
 
 # test = FilmReviews.new
-# test.retrieve_film_reviews('Goodfellas')
-# p test.print_all_film_reviews
+# test.retrieve_film_reviews('avengers')
+# p test.retrieve_mpaa_rating
--- spec/film_reviews_spec.rb
+++ spec/film_reviews_spec.rb
@@ -9,8 +9,64 @@ describe 'Latest film reviews' do
       @review.retrieve_film_reviews('avengers')
     end
 
-    it 'should return a hash when getting the data' do
-      expect(@review.print_all_film_reviews).to be_kind_of(Hash)
+    it 'should return a hash when getting the API data' do
+      expect(@review.retrieve_film_results).to be_kind_of(Hash)
+    end
+
+    it 'should return a string from status' do
+      expect(@review.retrieve_status).to be_kind_of(String)
+    end
+
+    it 'should return a string from copyright' do
+      expect(@review.retrieve_copyright).to be_kind_of(String)
+    end
+
+    # it 'should return a boolean from has_more' do
+    #   expect(@review.retrieve_has_more).to be equal 'false,'
+    # end
+
+    it 'should return a integer from num_results' do
+      expect(@review.retrieve_num_results).to be_kind_of(Integer)
+    end
+
+    it 'should return a array from results' do
+      expect(@review.retrieve_results).to be_kind_of(Array)
+    end
+
+    it 'should return all of the film titles from display results to be string' do
+      @review.retrieve_all_display_title.each do |title|
+        expect(title).to be_kind_of(String)
+      end
+    end
+
+    it 'should return all of the age ratings from mpaa_rating to be string' do
+      @review.retrieve_mpaa_rating.each do |rating|
+        expect(rating).to be_kind_of(String)
+      end
+    end
+ 
+    it 'should return all of the critics pick from critics pick to be Integer' do
+      @review.retrieve_critics_pick.each do |pick|
+        expect(pick).to be_kind_of(Integer)
+      end
+    end
+
+    it 'should return all of the bylines from byline to be String' do
+      @review.retrieve_byline.each do |line|
+        expect(line).to be_kind_of(String)
+      end
+    end
+
+    it 'should return all of the headlines from headline to be String' do
+      @review.retrieve_headline.each do |head|
+        expect(head).to be_kind_of(String)
+      end
+    end
+
+    it 'should return all of the short summaries from summary_short to be String' do
+      @review.retrieve_summary_short.each do |short|
+        expect(short).to be_kind_of(String)
+      end
     end
   end
 end
